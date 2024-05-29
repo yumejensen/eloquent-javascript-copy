86bc86e3133a0d8f16d76339c98c61e7ac2f3b59
alex-aaron <alexfrancisaaron@gmail.com>
2024-05-29 14:28:24 -0500

    added additional chapter 2 instructions

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -3,6 +3,29 @@
 // triangles ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+/*
+Directions: Create a function called `triangles` that takes in a parameter of a
+number. This number determines the "size" of the triangle you need to log. 
+HINT: each "level" of the triangle needs to be logged individually.
+
+example: triangles(3);
+LOGS =>
+
+#
+##
+###
+
+example: triangles(5);
+LOGS =>
+
+#
+##
+###
+####
+#####
+
+*/
+
 function triangles() {
   
 }
@@ -12,13 +35,51 @@ function triangles() {
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function fizzBuzz() {
+/*
+Directions: Create a function called fizzBuzz that takes in two parameters - 
+`start`, `end`. `start` and `end` both represent numbers. This function should
+access each number from `start` to `end` and log different statements depending
+on the number:
+
+  - if the number is divisible by 3, log "fizz"
+  - if the number is divisible by 5, log "buzz"
+  - if the number is divisible by both 3 & 5, log "fizzbuzz"
+  - if the number is not divisible by 3 or 5, log the number
+*/
+
+function fizzBuzz(start, end) {
   
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+
+/*
+Directions: Create a function called drawChessboard that takes in a parameter of
+`x` that represents the "size" of the chessboard you are going to log. The chessboard
+will be a collection of spaces and #'s creating the appearance of a chessboard.
+
+Note: in order to do this correctly, you need to ultimately create a string containing
+linebreak characters (\n). For example, if you invoke drawChessboard(3) it should log a
+string that looks like this " # \n# #\n # \n# #"
+
+example: drawChessboard(4);
+LOGS =>
+
+ # #
+# #
+ # #
+# #
+
+exampmle drawChessboard(3);
+LOGS =>
+
+ # 
+# #
+ #
+
+*/
 
 function drawChessboard(x) {
 
